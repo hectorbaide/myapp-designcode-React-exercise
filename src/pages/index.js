235b1fcddc2375fefc94d228babdeafbc235365b
Hector Baide <hectorbaide@gmail.com>
2018-08-06 13:12:58 +0200

    Continued course

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>I think I understand how this works...</h1>
-    <p>Wuuuut?</p>
-    <p>So this is react?!?</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>I think I understand how this works...</h1>
+        <p>Wuuuut?</p>
+        <p>So this is react?!? I wanna have a bit more text so we can see how stuff works eeeeh??</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
